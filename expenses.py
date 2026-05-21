369ef5ab2b15c2f9939c0c29aebba7f1eb6f1f02
basilath <basilathunnisha2007@gmail.com>
2026-05-21 11:21:07 +0530

    first commit

---
+++ expenses.py
@@ -0,0 +1,5 @@
+food=float(input("Enter the food expenses:"))
+travel=float(input("enter the travel expenses:"))
+shopping=float(input("enter the shopping expenses:"))
+total=food+travel+shopping
+print("total expenses:",total)
---
+++ product.py
@@ -0,0 +1,5 @@
+price=float(input("enter the price:"))
+if price>=1000:
+    print("expensive")
+else:
+    print("affordable")
